bf9c17dd0f34fc214cc5674a9f1c507803e73280
Leo Rowan <jakrazzi@gmail.com>
2017-07-07 11:31:23 -0400

    Add abbreviations for common node commands

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -x PATH $PATH $GOPATH/bin
 ### Aliases ###
 # Use Nvim if Exists
 if which nvim > /dev/null ^ /dev/null
-  alias vim "nvim"
+  alias vim 'nvim'
 end
 
 # Shortcuts
@@ -15,16 +15,21 @@ alias todo 'vim ~/.dotfiles/.todos.md'
 abbr proj 'cd ~/projects'
 abbr dotfiles 'cd ~/.dotfiles'
 abbr l 'ls -lah'
+abbr cl 'clear'
 
 # Git
-abbr gc "git clone"
-abbr gs "git status"
-abbr gd "git diff"
-abbr ga "git add"
-abbr gi "git commit"
-abbr gh "git log --pretty --graph --date short --oneline --color=always | less -r"
-abbr ggo "git checkout"
-abbr gb "git branch"
-abbr gpom "git push origin master"
+abbr gc 'git clone'
+abbr gs 'git status'
+abbr gd 'git diff'
+abbr ga 'git add'
+abbr gi 'git commit'
+abbr gh 'git log --pretty --graph --date short --oneline --color=always | less -r'
+abbr ggo 'git checkout'
+abbr gb 'git branch'
+abbr gpom 'git push origin master'
 abbr gprom 'git pull --rebase origin master'
 
+# Node
+abbr ni 'npm install'
+abbr ns 'npm start'
+abbr nt 'npm test'
